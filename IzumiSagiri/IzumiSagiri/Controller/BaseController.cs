e498e52abf5d80a89087bf3daca3425233c53490
IzumiSagiris <m99shoot@126.com>
2017-09-27 16:39:26 +0800

    IzumiSagiris First Step

---
+++ IzumiSagiri/IzumiSagiri/Controller/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace IzumiSagiri
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+
+        }
+
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+
+        }
+    }
+}
---
+++ IzumiSagiri/IzumiSagiri/Controller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IzumiSagiri
+{
+    /// <summary>
+    /// Home
+    /// </summary>
+    public class HomeController : BaseController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
