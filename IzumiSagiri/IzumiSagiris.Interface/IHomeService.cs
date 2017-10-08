066027bddba8bd32ddeef017b2bfd9d60070f4a0
IzumiSagris <m99shoot@126.com>
2017-10-08 21:36:21 +0800

    error commit

---
+++ IzumiSagiri/IzumiSagiris.Interface/IHomeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzumiSagiris.Interface
+{
+    public interface IHomeService
+    {
+        double Shimada(double one, double two);
+    }
+}
---
+++ IzumiSagiri/IzumiSagiris.Service/HomeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzumiSagiris.Service
+{
+    public class HomeService
+    {
+        public double Shimada(double one, double two)
+        {
+            return one * two;
+        }
+
+    }
+}
